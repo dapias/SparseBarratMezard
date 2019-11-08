3be5bb657011e29cf40634ab0b3920f133c41958
dapias <dandroveros@gmail.com>
2019-11-08 18:54:09 +0100

    create function

--- loc_cluster.jl
+++ loc_cluster.jl
@@ -1,15 +1,13 @@
 using JLD
 include("cavitymethod2.jl")
-N = 10^2
-nsteps = N*10^4
-T = 0.6
-lambda = -0.05
-epsilon2 = [1e-5,1e-3, 1e-4];
-c = 3
-rho_mean = mean(DOS(lambda, c, T, 10^3, 10^5, 10^7, epsilon2))
-ns = 2^17
-epsi = 8.0./(rho_mean.*ns);
-pop_epsi =  population_update(lambda, c, T, N, nsteps, epsi)
-save("localization/scaling/population/T=$(T)lambda=$(lambda)N=$(N)epsivariable.jld",
-     "n", ns, "pop", pop_epsi)
 
+function locincluster(lambda, T; N = 10^2, c = 3, epsilon2 =  [1e-5,1e-3, 1e-4])
+    nsteps = N*10^4
+    rho_mean = mean(DOS(lambda, c, T, 10^3, 10^5, 10^7, epsilon2))
+    ns = 2^17
+    epsi = 8.0./(rho_mean.*ns);
+    pop_epsi =  population_update(lambda, c, T, N, nsteps, epsi)
+    save("localization/scaling/population/T=$(T)lambda=$(lambda)N=$(N)epsivariable.jld",
+         "n", ns, "pop", pop_epsi)
+end
+    
