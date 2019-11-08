f6e7637a5c23aa0703a13fcc07637849153f4644
dapias <dandroveros@gmail.com>
2019-11-08 19:01:21 +0100

    parallel

--- loc_cluster.jl
+++ loc_cluster.jl
@@ -1,7 +1,8 @@
 using JLD
 include("cavitymethod2.jl")
 
-function locincluster(lambda, T; N = 10^2, c = 3, epsilon2 =  [1e-5,1e-3, 1e-4])
+addprocs(3)
+@everywhere function locincluster(lambda, T; N = 10^2, c = 3, epsilon2 =  [1e-5,1e-3, 1e-4])
     nsteps = N*10^4
     rho_mean = mean(DOS(lambda, c, T, 10^3, 10^5, 10^7, epsilon2))
     ns = 2^17
