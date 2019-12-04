e82ac40a9b5c485d8c6898764045a1213364ce80
dapias <dandroveros@gmail.com>
2019-12-04 09:33:26 +0100

    code to generate LT

--- src/SparseBarratMezard.jl
+++ src/SparseBarratMezard.jl
@@ -1,9 +1,11 @@
 module SparseBarratMezard
 
-using Distributions, LightGraphs, LinearAlgebra, SparseArrays, StatsBase
+using Distributions, LightGraphs, LinearAlgebra, SparseArrays, StatsBase,
+    InverseLaplace
 
 include("single_instance.jl")
 include("population_dynamics.jl")
 include("gillespie.jl")
+include("laplacetransform.jl")
 
 end
