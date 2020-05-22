47eb91e0e105423c3fc0826030008a74b728119d
dapias <dandroveros@gmail.com>
2020-05-22 20:48:02 +0200

    Preparing notebook

---
+++ cluster/bouchaud_dos_cluster.jl
@@ -0,0 +1,54 @@
+@everywhere include("../src/SparseBarratMezard.jl")
+@everywhere using .SparseBarratMezard
+@everywhere using JLD
+
+
+@everywhere function dos(lambda, T; N = 10^4, c = 5,
+             ensemble = 10^7, ep2 = [1e-3, 1e-4, 1e-5])
+    a = load("../data/populations/Bouchaud/T=$(T)/lambda=$(lambda)N=$(N)c=$(c)epsi_trescientos.jld")
+    pop = a["pop"]
+    
+    dos = DOS(lambda, c, T, N, ensemble, ep2, pop)
+
+    return dos
+end
+
+@everywhere function dos2(lambda, c; N = 10^4, T = 0.6,
+             ensemble = 10^7, ep2 = [1e-3, 1e-4, 1e-5])
+    a = load("../data/populations/Bouchaud/T=$(T)/lambda=$(lambda)N=$(N)c=$(c)epsi_trescientos.jld")
+    pop = a["pop"]
+    
+    dos = bouchaud_DOS(lambda, c, T, N, ensemble, ep2, pop)
+
+    return dos
+end
+
+
+#@everywhere l_grid = -exp10.(range(-3,stop =0, length = 101))
+#@everywhere cs = [5, 10,20,100]
+
+@everywhere cs = [5, 20, 100]
+@everywhere l_grid = collect(-1:0.01:0.)
+
+for c in cs
+   println(c)
+  dos_pp = pmap(x -> dos2(x,c), l_grid)
+    save("../data/dos/Bouchaud/T=0.6c=$(c).jld", "dos", dos_pp, "lambda",l_grid,  "ensemble", 10^7, "epsilon2", [1e-3, 1e-4, 1e-5])
+end
+
+
+#@everywhere l_grid = collect(-1:0.01:0.)
+#@everywhere l_grid = -exp10.(range(-3,stop =0, length = 101))
+#@everywhere T_grid  = [0.2, 0.35,0.5,0.8]
+
+#for T in T_grid
+#    println(T)
+#@everywhere T = 0.1
+#dos_pp = pmap(x -> dos(x,T), l_grid)
+#save("../data/dos/T=$(T)c=3.jld", "dos", dos_pp, "lambda",l_grid,
+  #   "ensemble", 10^7, "epsilon2", [1e-3, 1e-4, 1e-5])
+#end
+
+
+
+    
---
+++ cluster/bouchaud_pop_cluster.jl
@@ -0,0 +1,23 @@
+#using  Distributed
+#addprocs(3)
+
+@everywhere include("../src/SparseBarratMezard.jl")
+@everywhere using .SparseBarratMezard
+@everywhere using JLD
+
+@everywhere function pop_cluster(lambda, T, c; N = 10^4, epsilon = 1e-300)
+    nsteps = N*10^4
+    pop_epsi =  bouchaud_generate_population(lambda, c, T, N, nsteps, epsilon)
+    #save("../data/populations/T=$(T)/loglambda=$(lambda)N=$(N)c=$(c)epsi_trescientos.jld", "n", N, "pop", pop_epsi)
+    save("../data/populations/Bouchaud/T=$(T)/lambda=$(lambda)N=$(N)c=$(c)epsi_trescientos.jld", "n", N, "pop", pop_epsi)
+end
+
+@everywhere l_grid = collect(-1:0.01:0.)
+#@everywhere l_grid = -exp10.(range(-3,stop =0, length = 101))
+@everywhere T = 0.6
+@everywhere c = 100
+
+#mkdir("../data/populations/T=$(T)/")
+println("start")
+pmap(x -> pop_cluster(x,T, c), l_grid)
+
