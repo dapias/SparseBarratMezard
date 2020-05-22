4e3191d08262001d43daee4d8322d5009e80e007
dapias <dandroveros@gmail.com>
2020-05-22 20:59:48 +0200

    Minimal code

--- src/SparseBarratMezard.jl
+++ src/SparseBarratMezard.jl
@@ -4,13 +4,8 @@ using Distributions, LightGraphs, LinearAlgebra, SparseArrays, StatsBase,
     InverseLaplace
 
 include("single_instance.jl")
-#include("rescaled_single_instance.jl")
-#include("bouchaud_single_instance.jl")
 include("population_dynamics.jl")
-include("rescaled_population.jl")
-include("bouchaud_population_dynamics.jl")
-include("gillespie.jl")
-#include("bouchaud_gillespie.jl")
-include("laplacetransform.jl")
+#include("rescaled_population.jl")
+#include("laplacetransform.jl")
 
 end
