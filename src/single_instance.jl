edf0168f42f111d1a260d086486c98a557ce2e95
Diego Tapias <diegotapias@Peters-MacBook-Pro.local>
2022-12-05 12:32:44 +0100

    Make it efficient for Sparse arrays

--- src/single_instance.jl
+++ src/single_instance.jl
@@ -1,25 +1,32 @@
 export generate_sparse_barrat_matrix, generate_barrat_matrix, rho, DOSIPR, resolvent
 
 ###Generates the instances
-function generate_sparse_barrat_matrix(n::Int64,c::Int64, beta::Float64)
+function generate_sparse_barrat_elements(n::Int64,c::Int64)
     dene = Exponential()
     L = random_regular_graph(n,c)
     adj = adjacency_matrix(L);
     energies = rand(dene,nv(L));
-    M = 1.0*adj;
-    l_nei = L.fadjlist;
+    #M = 1.0*adj;
+    l_nei = LightGraphs.SimpleGraphs.fadj(L)
 
+    return energies, l_nei, adj, L
+end
+
+function generate_sparse_barrat_matrix(energies::Array, list_nei::Array, adj::SparseMatrixCSC, n::Int64, c::Int64, T::Float64)
+
+    beta = 1/T
+    M = 1.0*adj
     a = zeros(n)
     
     for i in 1:n
         for j in 1:c
-            k = l_nei[i][j]
+            k = list_nei[i][j]
             M[i,k] = 1.0/c*(1.0/(1.0+exp(-beta*(energies[i] - energies[k]))))
             a[k] += M[i,k]
         end
     end
     
-    for i in vertices(L)
+    for i in 1:n
         M[i,i] = -a[i]
     end
     
@@ -29,95 +36,95 @@ function generate_sparse_barrat_matrix(n::Int64,c::Int64, beta::Float64)
     p_dir = peq.^(1/2)
     Ms = Diagonal(p_inv)*M *Diagonal(p_dir);
 
-
-    return Ms, energies, l_nei
 end
 
-function generate_barrat_matrix(n::Int64,c::Int64, beta::Float64)
+function generate_barrat_matrix(energies::Array, list_nei::Array, adj::SparseMatrixCSC, n::Int64, c::Int64, T::Float64)
 
-    Ms, energias, nei = generate_sparse_barrat_matrix(n,c,beta)
-
+    Ms = generate_sparse_barrat_matrix(energies, list_nei, adj, n, c, T)
     Ms = Matrix(Symmetric(Ms))
     
-    return Ms, energias, nei
+    return Ms
 end
 
 ####Cavity system
+function omega_generator(adj::SparseMatrixCSC{Int64,Int64}, n::Int64, c::Int64)
+    col = repeat(1:n, inner = c)
+    sparse(adj.rowval, col, rand(Complex{Float64}, c*n), n, n)
+end
+
 function symmetric_f(e1::Float64, beta::Float64, e2::Float64)
     exp(beta*(e1 + e2)/2)/(2*cosh(beta*(e1-e2)/2.)) 
 end
 
 function local_error(lambda::Float64,  neighs::Array{Array{Int64,1},1},
-        Omega_old::SparseMatrixCSC{Complex{Float64},Int64}, epsilon::Float64,  beta::Float64, energies::Array{Float64,1}, c::Int64, fs::SparseMatrixCSC{Complex{Float64},Int64}, N::Int64, Omega_sum::SparseMatrixCSC{Complex{Float64},Int64})
+        Omega_old::SparseMatrixCSC{Complex{Float64},Int64}, epsilon::Float64,  beta::Float64, energies::Array{Float64,1}, n::Int64, Omega_sum::SparseMatrixCSC{Complex{Float64},Int64}, c::Int64)
 
-    for k in 1:N
+    for k in 1:n
         value = neighs[k]
         for j in value
+            a = 0.0 +0.0*im
             for l in value
                 if l != j
-                    Omega_sum[k, j] += im*fs[k,l]*Omega_old[l, k]/(im*fs[k,l] +Omega_old[l,k])
+                    fs = symmetric_f(energies[k], beta, energies[l])
+                    a+= im*fs*Omega_old[l,k]/(im*fs + Omega_old[l,k])
                 end
             end
-            Omega_sum[k,j] += im*(lambda - im*epsilon)/(exp(-beta*energies[k])/c)
+            @inbounds Omega_sum[k,j] = a + im*(lambda - im*epsilon)/(exp(-beta*energies[k])/c)
         end
     end
-    ###Two ways of computing the error: Absolute or relative. The absolute demands less memory (used here)
-    #        reg = 10^-10.
-    #relative_real = real.(Omega_sum .- Omega_old)./(abs.(real.(Omega_old))  .+ reg)
-    #relative_imag = imag.(Omega_sum .- Omega_old)./(abs.(imag.(Omega_old)) .+ reg)
-    #max_real = maximum(abs.(relative_real))
-    #max_imag = maximum(abs.(relative_imag))
-    #error = maximum([max_real, max_imag])
+  
     error = sum(abs.(Omega_sum .- Omega_old))  
 
-    @inbounds fill!(Omega_old, zero(Complex{Float64}))
+ #   @inbounds fill!(Omega_old, zero(Complex{Float64}))
 
     return error, Omega_sum, Omega_old
 end
 
-function rho(lambda::Float64, c::Int64, beta::Float64, epsilon::Float64,  energies::Array{Float64,1}, nei::Array{Array{Int64,1},1}; tolerance = 0.1)
-##The Barrat Matrix is not a needed  argument. The energies and the list of neighbours are enough information to apply the cavity method.
-    n = length(energies)
+function cavity_omegas(lambda::Float64, c::Int64, epsilon::Float64,  energies::Array{Float64,1}, nei::Array{Array{Int64,1},1},
+                       adj::SparseMatrixCSC{Int64,Int64}, T::Float64; tolerance = 0.1)
+    ##The Barrat Matrix is not a needed  argument. The energies and the list of neighbours are enough information to apply the cavity method. The returned are the cavity_omegas
+
+    beta = 1/T
     error2 = 10.0*tolerance*n*n
-    Omegas = spzeros(Complex{Float64}, n, n)   
-    fs = spzeros(Complex{Float64}, n, n)
+    Omegas = omega_generator(adj, n, c)
+    Omegap = similar(Omegas)
     
-    for key in 1:n
-        value = nei[key]
-        for i in value
-            Omegas[key, i] = rand(Complex{Float64})
-            fkeyi = symmetric_f(energies[i], beta, energies[key])
-            fs[key,i] = fs[i,key] = fkeyi
-        end
-    end
-
-    Omegap = spzeros(Complex{Float64}, n, n)
-
     while error2 > tolerance
-         error2, Omegas, Omegap = local_error(lambda, nei,  Omegas, epsilon,  beta, energies, c, fs, n, Omegap)
+         error2, Omegas, Omegap = local_error(lambda, nei,  Omegas, epsilon,  beta, energies, n, Omegap, c)
     end
     
-    sum_var = 0.
+    return Omegas
+end
+
+function marginal_gs(lambda::Float64, c::Int64, epsilon::Float64, energies::Array{Float64,1}, nei::Array{Array{Int64,1},1}, n::Int64, Omegas::SparseMatrixCSC{Complex{Float64},Int64}, T::Float64)
+
+    beta = 1/T
+    mars = zeros(n)
+
     for j in 1:n
-        sum_j = 0.
+        sum_j = 0.0
         for k in nei[j]
-            sum_j += im*Omegas[k,j]*fs[k,j]/( im*fs[k,j] + Omegas[k,j])
+            fs = symmetric_f(energies[k], beta, energies[j])
+            @inbounds sum_j +=  im*fs*Omegas[k,j]/(im*fs + Omegas[k,j])
         end
-        
-        omega_j = im*(lambda - epsilon*im)/(exp(-beta*energies[j])/c) + sum_j
-        sum_var +=  real( (1/(omega_j*exp(-beta*energies[j])/c)))
+
+        omega_j =  im*(lambda - im*epsilon)/(exp(-beta*energies[j])/c) + sum_j
+        mars[j] = real(exp(beta*energies[j])/(c*omega_j))
     end
-    
-    return 1/(pi*n)*sum_var
-    
+
+    mars
 end
 
-function rho(lambda::Float64, c::Int64, n::Int64, beta::Float64, epsilon::Float64; tolerance = 0.1)
-    A, energies, nei  = generate_sparse_barrat_matrix(n,c, beta)
+function dens(lambda::Float64, c::Int64, epsilon::Float64, energies::Array{Float64,1}, nei::Array{Array{Int64,1},1}, n::Int64, Omegas::SparseMatrixCSC{Complex{Float64},Int64}, T::Float64)
 
-    rho(lambda, c,  beta, epsilon, A, energies, nei)
-    
+    sum(marginal_gs(lambda, c, epsilon, energies, nei, n, Omegas, T))/(pi*n)
+
 end
+
+
+        
+
+
 
 
 function DOSIPR(lambda::Float64, c::Int64, n::Int64, beta::Float64, epsilon::Float64; tolerance = 0.1)
